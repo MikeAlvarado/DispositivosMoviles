6848457939db401d9d7c9b72b52dfd291851f71f
ferpompa <pomps1996@gmail.com>
2017-10-22 19:30:28 -0500

    Se agrego el button de favoritos y nombre del nivel

---
+++ LSM/LSM/ViewControllerNiveles.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerNiveles.swift
+//  LSM
+//
+//  Created by Orange Investments on 10/22/17.
+//  Copyright © 2017 Jorge Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerNiveles: UIViewController {
+
+    @IBOutlet weak var lbNivel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lbNivel.text = "Nivel\nAbecedario"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
