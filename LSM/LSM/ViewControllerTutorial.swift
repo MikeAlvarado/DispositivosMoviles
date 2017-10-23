114978ca849cd358cffe646cef202980f71460e4
ferpompa <pomps1996@gmail.com>
2017-10-22 21:40:40 -0500

    Agregue tutorial

--- LSM/LSM/ViewControllerTutorial.swift
+++ LSM/LSM/ViewControllerTutorial.swift
@@ -11,15 +11,13 @@ import UIKit
 class ViewControllerTutorial: UIViewController {
     
     
-    @IBOutlet weak var lbTitulo: UILabel!
-    @IBOutlet weak var lbPuntos: UILabel!
-    @IBOutlet weak var lbNivel: UILabel!
-    @IBOutlet weak var uiImagen: UIImageView!
-    @IBOutlet weak var btSiguiente: UIButton!
-    @IBOutlet weak var btRegresa: UIButton!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Tutorial"
+        
 
         // Do any additional setup after loading the view.
     }
