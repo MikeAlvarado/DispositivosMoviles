2f9d26826fff5a1490f4b0ad14756397eecd18a0
ferpompa <pomps1996@gmail.com>
2017-10-22 20:24:47 -0500

    Se agrego un nuevo viewController para cambiar el nombre del jugador

---
+++ LSM/LSM/ViewControllerCambiaNombre.swift
@@ -0,0 +1,51 @@
+//
+//  ViewControllerCambiaNombre.swift
+//  LSM
+//
+//  Created by Orange Investments on 10/22/17.
+//  Copyright © 2017 Jorge Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerCambiaNombre: UIViewController {
+
+    @IBOutlet weak var tfNombre: UITextField!
+    @IBOutlet weak var btContinuar: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let colorTop =  UIColor(red: 255.0/255.0, green: 163.0/255.0, blue: 26.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [ colorTop, colorBottom]
+        gradientLayer.locations = [ 0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func btContinuar(_ sender: UIButton)
+    {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
